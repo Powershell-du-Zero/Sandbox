d2758c170a1b4317ad37b98a33561bbf911f70c0
Thomas ILLIET <contact@thomas-illiet.fr>
2019-05-12 13:13:20 +0200

    ✨ Add SandboxConfig Class

---
+++ Src/Lib/SandboxConfig.Class.ps1
@@ -0,0 +1,37 @@
+Class SandboxConfig
+{
+    #Region Properties
+    # Enable or disable networking in the sandbox
+    hidden [SandboxStatus]$Networking
+
+    # Enable or disable the virtualized GPU
+    hidden [SandboxStatus]$VGpu
+
+    # List of script or program executions at startup
+    hidden [System.Collections.Generic.List[SandboxLogonCommand]]$LogonCommand
+
+    # List of shared folders of the host
+    hidden [System.Collections.Generic.List[SandboxMappedFolder]]$MappedFolder
+    #endregion
+
+    #region Constructors
+    SandboxConfig()
+    {
+        $this.Networking = [SandboxStatus]::Enable
+        $this.VGpu = [SandboxStatus]::Disable
+        $this.LogonCommand = [System.Collections.Generic.List[SandboxLogonCommand]]::new()
+        $this.MappedFolder = [System.Collections.Generic.List[SandboxMappedFolder]]::new()
+    }
+
+    SandboxConfig(
+        [SandboxStatus]$Networking,
+        [SandboxStatus]$VGpu
+    )
+    {
+        $this.Networking = [SandboxStatus]::Enable
+        $this.VGpu = [SandboxStatus]::Disable
+        $this.LogonCommand = [System.Collections.Generic.List[SandboxLogonCommand]]::new()
+        $this.MappedFolder = [System.Collections.Generic.List[SandboxMappedFolder]]::new()
+    }
+    #endregion
+}
